15a8c2a1a3b66e106614b3a1d98b473215250125
knightliao <knightliao@gmail.com>
2015-05-22 15:09:11 +0800

    re

---
+++ pikaq-demos/pikaq-web-demo/src/main/java/com/baidu/pika/demo/web/handler/FreeMarkerController.java
@@ -0,0 +1,51 @@
+package com.baidu.pika.demo.web.handler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by knightliao on 15/1/20.
+ */
+@Controller
+@RequestMapping("/home")
+public class FreeMarkerController {
+
+    public static class User {
+        private String username;
+        private String password;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+
+    @RequestMapping("/index")
+    public ModelAndView Add(HttpServletRequest request, HttpServletResponse response) {
+
+        User user = new User();
+        user.setUsername("zhangsan");
+        user.setPassword("1234");
+        List<User> users = new ArrayList<User>();
+        users.add(user);
+        return new ModelAndView("index", "users", users);
+    }
+}
---
+++ pikaq-demos/pikaq-web-demo/src/main/webapp/WEB-INF/jsp_views/index.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" pageEncoding="utf-8" %>
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="#">
+
+    <title>pikaq-web-demo</title>
+</head>
+
+<body screen_capture_injected="true">
+
+
+</body>
+</html>

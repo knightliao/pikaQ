1a10b7b02b0c38d0a59b80c478971a61e62d8c98
knightliao <knightliao@gmail.com>
2015-06-03 11:13:44 +0800

    re

---
+++ pikaq-client/src/main/java/com/baidu/pikaq/client/TestMain.java
@@ -0,0 +1,16 @@
+package com.baidu.pikaq.client;
+
+import java.util.Arrays;
+
+/**
+ * Created by knightliao on 15/5/27.
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+
+        String dd = "kkk";
+
+        System.out.println(Arrays.asList(dd));
+    }
+}
---
+++ pikaq-demos/pikaq-standalone-demo/src/main/java/com/baidu/pikaq/demo/origin/producer/TestMain.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2015 Baidu, Inc. All Rights Reserved.
+ */
+package com.baidu.pikaq.demo.origin.producer;
+
+import java.util.Arrays;
+
+/**
+ * Created by knightliao on 15/5/27.
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+
+        String dd = "kkk";
+
+        System.out.println(Arrays.asList(dd));
+    }
+}

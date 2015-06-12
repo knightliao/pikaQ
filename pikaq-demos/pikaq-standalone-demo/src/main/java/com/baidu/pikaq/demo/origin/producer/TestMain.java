ca45ae5bd7437833efbdbcb7f1f51c1c513d9c37
knightliao <knightliao@gmail.com>
2015-06-12 18:27:09 +0800

    use rabbitmq to log

--- pikaq-demos/pikaq-standalone-demo/src/main/java/com/baidu/pikaq/demo/origin/producer/TestMain.java
+++ pikaq-demos/pikaq-standalone-demo/src/main/java/com/baidu/pikaq/demo/origin/producer/TestMain.java
@@ -5,15 +5,30 @@ package com.baidu.pikaq.demo.origin.producer;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by knightliao on 15/5/27.
  */
 public class TestMain {
+
+    protected static final Logger LOGGER = LoggerFactory.getLogger(TestMain.class);
 
     public static void main(String[] args) {
 
         String dd = "kkk";
 
         System.out.println(Arrays.asList(dd));
+
+        for (int i = 0; i < 1000000; ++i) {
+            LOGGER.info(String.valueOf(i));
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }

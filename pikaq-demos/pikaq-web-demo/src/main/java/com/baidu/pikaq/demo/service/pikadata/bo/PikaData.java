ba21125ff0ded5fef9c0c308bfe294cc378e3d23
knightliao <knightliao@gmail.com>
2015-07-21 13:51:09 +0800

    re

--- pikaq-demos/pikaq-web-demo/src/main/java/com/baidu/pikaq/demo/service/pikadata/bo/PikaData.java
+++ pikaq-demos/pikaq-web-demo/src/main/java/com/baidu/pikaq/demo/service/pikadata/bo/PikaData.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 
 import com.baidu.pikaq.core.db.store.StoreDataStatusEnum;
 import com.baidu.pikaq.demo.service.DB;
-import com.baidu.pikaq.demo.service.campaign.bo.Campaign;
 import com.baidu.pikaq.utils.DateUtils;
 import com.baidu.ub.common.commons.BaseObject;
 import com.baidu.unbiz.common.genericdao.annotation.Column;
@@ -19,7 +18,7 @@ import com.baidu.unbiz.common.genericdao.annotation.Table;
 @Table(db = DB.DB_NAME, name = "pikaq_data", keyColumn = "id")
 public class PikaData extends BaseObject<Long> {
 
-    protected static final Logger LOG = LoggerFactory.getLogger(Campaign.class);
+    protected static final Logger LOG = LoggerFactory.getLogger(PikaData.class);
 
     public static final String PIKA_DATA_COLUMN_CORRELATION = "correlation";
 
